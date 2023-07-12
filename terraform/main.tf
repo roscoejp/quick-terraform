f55cb0c227d5cccb4c0c1e97b9acef70e71f56af
Roscoe Pyell <roscoejp@gmail.com>
2023-07-12 13:28:05 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,31 @@
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
+# Upload a text file as an object to the storage bucket
+resource "google_storage_bucket_object" "default" {
+  name         = "sample.txt"
+  source       = "../sample.txt"
+  content_type = "text/plain"
+  bucket       = "boring-hoover"
 }
 
-output "ip" {
-  value = data.http.myip.body
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "boring-hoover"
+    prefix = "terraform/state"
+  }
+}
+
+# define variables
+variable "project_id" {
+  type        = string
+  description = "The project name to use."
+}
+
+variable "region" {
+  type        = string
+  description = "The region used for the provider."
+  default     = "us-east1"
 }
